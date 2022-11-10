b161d6c44a1466816f95e4ebf01443ddcc6ab09a
Rod Bennett <rodbennett@Rods-Air-2.lan>
2022-11-09 21:05:21 -0700

    react-icons installed, gh and linked added

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,10 +1,21 @@
 import github from '../images/github.png'
+import linkedin from '../images/linkedin.png'
+import '../styles/Footer.css'
+import { FaGithub } from 'react-icons/fa'
+import { FaLinkedin } from 'react-icons/fa'
+
+
 
 const Footer = () => {
   return (
-    <div>
-        <a href="https://github.com/RodBennett">
-      <img src={github} alt="github icon" />
+    <div className='footer-container'>
+        <i><FaGithub /> </i>
+        <i><FaLinkedin /> </i>
+        <a className="github-icon" href="https://github.com/RodBennett">
+      <img src={github} alt="Github Icon" />
+      </a>
+      <a className="linkedin-icon" href="#">
+      <img src={linkedin} alt="Linkedin Logo" />
       </a>
     </div>
   )
