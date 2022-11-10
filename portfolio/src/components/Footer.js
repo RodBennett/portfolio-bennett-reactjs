99216bbf2d15bfe28d29578107c9eab0bac7c139
Rod Bennett <rodbennett@Rods-Air-2.lan>
2022-11-09 21:07:26 -0700

    react-icons installed, <a> wrapped around icons

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,6 +1,6 @@
-import github from '../images/github.png'
-import linkedin from '../images/linkedin.png'
-import '../styles/Footer.css'
+// import github from '../images/github.png'
+// import linkedin from '../images/linkedin.png'
+// import '../styles/Footer.css'
 import { FaGithub } from 'react-icons/fa'
 import { FaLinkedin } from 'react-icons/fa'
 
@@ -9,14 +9,13 @@ import { FaLinkedin } from 'react-icons/fa'
 const Footer = () => {
   return (
     <div className='footer-container'>
-        <i><FaGithub /> </i>
-        <i><FaLinkedin /> </i>
         <a className="github-icon" href="https://github.com/RodBennett">
-      <img src={github} alt="Github Icon" />
-      </a>
-      <a className="linkedin-icon" href="#">
-      <img src={linkedin} alt="Linkedin Logo" />
-      </a>
+        <i><FaGithub /> </i>
+        </a>
+        <a className="linkedin-icon" href="#">
+        <i><FaLinkedin /> </i>
+        </a>
+    
     </div>
   )
 }
