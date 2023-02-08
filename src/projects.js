5e6eec9ce125cf02970b8d5af0c3cb2844fc49f6
RodBennett <rod.bennett75@gmail.com>
2023-02-07 21:42:55 -0700

    integral solutions project added

--- src/projects.js
+++ src/projects.js
@@ -2,46 +2,46 @@
 const projects = [
     {
         id: 1,
+        link: "https://integral-solutions-beta.herokuapp.com",
+        title: "Integral Solutions",
+        github: "https://github.com/RodBennett/integral-solutions-fork",
+        image: require("./images/integral-solutions.png"),
+    },
+    {
+        id: 2,
         link: "https://team-handbook.herokuapp.com/",
         title: "Team Handbook",
         github: "https://github.com/dpk5e7/shakespeares-monkeys",
         image: require("./images/team-handbook.png"),
     },
     {
-        id: 2,
+        id: 3,
         link: "https://hikamp.herokuapp.com/",
         title: "HiKamp",
         github: "https://github.com/stevecalla/hiKamp",
         image: require("./images/hikamp.png"),
     },
     {
-        id: 3,
+        id: 4,
         link: "https://drewlovato.github.io/project1-nationalparks/",
         title: "National Park",
         github: "https://github.com/drewlovato/project1-nationalparks",
         image: require("./images/nps.png"),
     },
     {
-        id: 4,
+        id: 5,
         link: "https://rodbennett.github.io/06-weather-app-challenge/",
         title: "Weather Forecast",
         github: "https://github.com/RodBennett/06-weather-app-challenge",
         image: require("./images/weather.png"),
     },
     {
-        id: 5,
+        id: 6,
         link: "https://rb-tech-blog.herokuapp.com/",
         title: "Tech Blog",
         github: "https://github.com/RodBennett/14-Tech-Blog",
         image: require("./images/blog.png"),
-    },
-    {
-        id: 6,
-        link: "https://pwa-text-editor-rb.herokuapp.com/",
-        title: "Text Editor",
-        github: "https://github.com/RodBennett/19-pwa-text-editor",
-        image: require("./images/text-editor.png"),
-    },
+    }
 ];
 
 export default projects;
